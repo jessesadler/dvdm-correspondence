225850968c79d4bd839297e52fdbdb02757e5b25
Jesse Sadler <jsadler@ucla.edu>
2017-06-20 17:30:29 -0700

    Bug fixes for shiny app
    Use if statements in routes function and label functions so that map will render when filter of year function returns empty data frame.

--- shiny-app-lines/server.R
+++ shiny-app-lines/server.R
@@ -1,9 +1,10 @@
+### Shiny Leaflet Map of Daniel van der Meulen Correspondence ###
 
 library(shiny)
 library(tidyverse)
 library(sp)
+library(sf)
 library(geosphere)
-library(data.table)
 library(leaflet)
 library(htmltools)
 library(RColorBrewer)
@@ -12,7 +13,10 @@ library(RColorBrewer)
 letters <- read_csv("data/dvdm-correspondence-1591.csv")
 locations <- read_csv("data/locations-1591.csv")
 geo_data <- select(locations, place:lat) # simplify locations data to only necessary variables
+# empty sf object of lines to use for periods with no data
+empty_sf <- read_rds("data/empty_sf.rds")
 
+# Set baseline for palette to be used for legend
 routes_all <- letters %>% 
   group_by(source, destination) %>%
   summarise(count = n()) %>%
@@ -25,12 +29,17 @@ shinyServer(function(input, output, session) {
   gcircles_routes <- reactive({
 
     per_route <- letters %>%
-      filter(year >= input$range[1] & year <= input$range[2]) %>% 
+      filter(year >= input$range[1] & year < input$range[2]) %>% 
       group_by(source, destination) %>%
       summarise(count = n()) %>%
       remove_missing() %>%
       arrange(count) %>% 
       ungroup()
+    
+    # Skip rest of function and return empty sf lines object if filtered data is empty
+    if(nrow(per_route) < 1) {
+      empty_sf
+    } else {
     
     # Join data to locations data
     geo_per_route <- per_route %>%
@@ -38,7 +47,6 @@ shinyServer(function(input, output, session) {
       left_join(geo_data, by = c("destination" = "place"))
     geo_per_route$ID <- as.character(c(1:nrow(geo_per_route))) # create id for each pair
     
-    # Extract source and destination lat and lon data to be placed into gcIntermediate command
     source_loc <- select(geo_per_route, lon.x, lat.x)
     dest_loc <- select(geo_per_route, lon.y, lat.y)
     
@@ -47,45 +55,40 @@ shinyServer(function(input, output, session) {
     
     # Convert a SpatialLines object into SpatialLinesDataFrame, so that tabular data can be added
     
-    # create empty dataframe
     ids <- data.frame()
-    
-    # fill dataframe with IDs for each line
+
     for (i in (1:length(routes))) {         
       id <- data.frame(routes@lines[[i]]@ID)
       ids <- rbind(ids, id)  }
     
-    colnames(ids)[1] <- "ID" # rename ID column, [1] says the first column is that which is to be renamed
-    
-    # Convert SpatialLines into SpatialLinesDataFrame using IDs as the dataframe
-    # Only variable in the SpatialLinesDataFRame after this is ID
+    colnames(ids)[1] <- "ID"
     
     routes <- SpatialLinesDataFrame(routes, data = ids, match.ID = TRUE)
     
-    ### Join data as SpatialLinesDataFrame and save ###
-    
+    # Create and return a SpatialLinesDataFrame
     merge(routes, geo_per_route, by = "ID")
+    }
   })
     
   ## Cities
   cities <- reactive({
   
     per_source <- letters %>%
-      filter(year >= input$range[1] & year <= input$range[2]) %>% 
+      filter(year >= input$range[1] & year < input$range[2]) %>% 
       group_by(source) %>%
       summarise(count = n()) %>%
       remove_missing() %>%
       arrange(count)
     
     per_destination <- letters %>%
-      filter(year >= input$range[1] & year <= input$range[2]) %>% 
+      filter(year >= input$range[1] & year < input$range[2]) %>% 
       group_by(destination) %>%
       summarise(count = n()) %>%
       remove_missing() %>%
       arrange(count)
     
     corrs_per <- letters %>%
-      filter(year >= input$range[1] & year <= input$range[2]) %>% 
+      filter(year >= input$range[1] & year < input$range[2]) %>% 
       group_by(source) %>%
       summarise(correspondents = n_distinct(name)) %>% 
       rename(place = source) %>% 
@@ -93,23 +96,44 @@ shinyServer(function(input, output, session) {
     
     geo_per_destination <- inner_join(geo_data, per_destination, by = c("place" = "destination"))
     geo_per_source <- inner_join(geo_data, per_source, by = c("place" = "source"))
-    cities_temp <- full_join(geo_per_source, geo_per_destination, by = "place") # keep all items in both tables
+    cities_temp <- full_join(geo_per_source, geo_per_destination, by = "place")
     
     left_join(cities_temp, corrs_per, by = "place") %>% 
       replace_na(list(count.x =0, count.y = 0, correspondents = 0)) # replace NAs with 0s in count columns
   })  
  
+  # Output base map
   output$map <- renderLeaflet({
     leaflet() %>% addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>% 
       setView(3.5, 46.5, zoom = 5)
-      
   })
-    
+
+  # Legend
   observe({
-    label2 <- sprintf(
+    pal <- colorNumeric(palette = "YlOrRd", domain = routes_all$count)
+    
+    leafletProxy("map", data = routes_all) %>%
+      clearControls() %>% 
+      addLegend(position = "topright",
+                colors = c("#ffd24d", "#addd8e"),
+                labels = c("Sent Location", "Received Location"),
+                opacity = 1) %>%
+      addLegend(pal = pal, values = ~count, opacity = 1,
+                title = "Letters<br/>Received")
+  })
+  
+  # CircleMarkers
+  observe({
+    # Return NA for labels if cities function is empty otherwise create labels
+    label2 <- if(nrow(cities()) < 1) {
+      NA
+    } else {
+    
+    sprintf(
       "<strong>%s</strong><br/>Letters from: %g<br/>Letters to: %g<br/>Correspondents: %g",
       cities()$place, cities()$count.x, cities()$count.y, cities()$correspondents
     ) %>% lapply(htmltools::HTML)
+    }
 
     leafletProxy("map", data = cities()) %>%
       clearMarkers() %>% 
@@ -121,32 +145,25 @@ shinyServer(function(input, output, session) {
                        label = label2)
   })
   
+  # Routes
   observe({
-
     pal <- colorNumeric(palette = "YlOrRd", domain = routes_all$count)
-    label1 <- sprintf(
+    
+    # Return NA for labels if routes function is empty otherwise create labels
+    label1 <- if(nrow(gcircles_routes()) < 1) {
+      NA
+    } else {
+    
+    sprintf(
       "%s to %s<br/>Number of letters: %g",
       gcircles_routes()$source, gcircles_routes()$destination, gcircles_routes()$count
     ) %>% lapply(htmltools::HTML)
+    }
     
     leafletProxy("map") %>% 
       clearShapes() %>% 
       addPolylines(data = gcircles_routes(), opacity = 0.9, weight = 3, color = ~pal(count),
                    label = label1,
                    highlight = highlightOptions(weight = 5, color = "red", opacity = 1))
-  })
-  
-  observe({
-    pal <- colorNumeric(palette = "YlOrRd", domain = routes_all$count) # may want to change this to make legend always the same
-    
-    leafletProxy("map", data = routes_all) %>%
-      clearControls() %>% 
-      addLegend(position = "topright",
-                colors = c("#ffd24d", "#addd8e"),
-                labels = c("Sent Location", "Received Location"),
-                opacity = 1) %>%
-      addLegend(pal = pal, values = ~count, opacity = 1,
-                title = "Letters<br/>Received")
-    
   })
   })
--- shiny-app-lines/ui.R
+++ shiny-app-lines/ui.R
@@ -4,8 +4,8 @@ library(leaflet)
 shinyUI(bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
-  absolutePanel(top = 10, left = 50,
-                sliderInput("range", "Years", min(1578), max(1591),
+  absolutePanel(top = 0, left = 60,
+                sliderInput("range", h3(style = "color:white", "Year Range"), min(1578), max(1592),
                             value = range(1578, 1579), step = 1, sep = "", animate=TRUE))
 )
 )
