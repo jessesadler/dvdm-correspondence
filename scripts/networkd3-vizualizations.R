ed37edb27c80415de405ec51714e3abf0fe283a2
Jesse Sadler <jsadler@ucla.edu>
2017-08-08 12:52:27 -0700

    Networkd3 vizualizations script

---
+++ scripts/networkd3-vizualizations.R
@@ -0,0 +1,42 @@
+## Network visualizations ##
+
+library(tidyverse)
+library(networkD3)
+
+letters <- read_csv("data/dvdm-correspondence-1591.csv")
+
+per_route <- letters %>%  
+  group_by(source, destination) %>%
+  summarise(count = n()) %>%
+  remove_missing() %>%
+  arrange(desc(count)) %>% 
+  ungroup()
+
+# Distinct sources and destinations into a single tibble with id column
+sources <- per_route %>%
+  distinct(source) %>%
+  rename(place = source)
+
+destinations <- per_route %>%
+  distinct(destination) %>%
+  rename(place = destination)
+
+nodes <- full_join(sources, destinations)
+places <- nrow(nodes)
+nodes <- add_column(nodes, id = 0:(places -1))
+
+# Create links with ids for source and destination
+links <- per_route %>% 
+  left_join(nodes, by = c("source" = "place")) %>% 
+  rename(source_id = id) %>% 
+  left_join(nodes, by = c("destination" = "place")) %>% 
+  rename(dest_id = id)
+
+# Forced Network graph
+forceNetwork(Links = links, Nodes = nodes, Source = "source_id", Target = "dest_id", 
+             Value = "count", NodeID = "place", Group = "id",
+             fontSize = 14, opacity = .8, zoom = TRUE)
+
+# Sankey Graph
+sankeyNetwork(Links = links, Nodes = nodes, Source = "source_id", Target = "dest_id", 
+              Value = "count", NodeID = "place", fontSize = 16, unit = "letter(s)")
