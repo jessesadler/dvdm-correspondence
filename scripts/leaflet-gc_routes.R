16f6f718806ef04597550133ee8a9bf286ded71b
Jesse Sadler <jrsadler@icloud.com>
2025-03-03 16:03:39 -0500

    Leaflet with all data

--- scripts/leaflet-gc_routes.R
+++ scripts/leaflet-gc_routes.R
@@ -1,38 +1,58 @@
 ### Leaflet map with sp great circle routes ###
 
 # Load libraries
-library(tidyverse)
+library(readr)
+library(dplyr)
+library(tidyr)
 library(sf)
 library(leaflet)
 library(htmltools)
 
 # Load letters and geographic data
-letters <- read_csv("data-raw/dvdm-correspondence-1591.csv")
-locations <- read_csv("data/locations-1591.csv") |> 
-  select(place:lat) # simplify locations data to only necessary variables
-gcircles_routes <- read_sf("data/routes_sf.geojson")
+letters <- read_csv("data-raw/letters.csv", col_types = list(
+  date = col_date("%Y%m%d"),
+  rec_date = col_date("%Y%m%d"),
+  resp_date = col_date("%Y%m%d")
+))
+locations <- read_csv("data/locations-geo.csv")
+gcircles_routes <- read_sf("data/routes-sf.geojson")
+
+# Filter by date if desired
+# letters <- letters |> 
+#   filter(year < 1592)
 
 # Prepare the data to be joined together
 # Calculate number of letters for each variable
-# Rename location columns to place so they can be joined together
+# Rename location columns to placename so they can be joined together
 # Name count column after variable they represent
-# Thus, source goes from being a place to a count etc.
+# Thus, source goes from being a placename to a count etc.
+per_route <- letters |> 
+  count(source, destination) |> 
+  drop_na() |> 
+  arrange(n)
+
 per_source <- letters |>
-  rename(place = source) |> 
-  group_by(place) |>
+  rename(placename = source) |> 
+  group_by(placename) |>
   summarise(source = n(),
             correspondents = n_distinct(writer)) |>
   drop_na()
 
 per_destination <- letters |>
-  rename(place = destination) |> 
-  group_by(place) |>
+  rename(placename = destination) |> 
+  group_by(placename) |>
   summarise(destination = n()) |>
   drop_na()
 
+# Make above into geo data
+
+routes <- per_route |> 
+  left_join(gcircles_routes, by = join_by(source, destination)) |> 
+  st_as_sf()
+
 # Join the three above data frames together and join with location data
-cities <- full_join(per_source, per_destination, by = "place") |> # keep all items in both tables
-  left_join(locations, by = "place") |> 
+cities <- full_join(per_source, per_destination, by = "placename") |> # keep all items in both tables
+  left_join(locations, by = "placename") |> 
   replace_na(list(source = 0, destination = 0, correspondents = 0))
     # replace NAs with 0s in count columns
 
@@ -42,32 +62,32 @@ sources <- filter(cities, source > 0)
 destinations <- filter(cities, destination > 0)
 
 # Color palette
-pal <- colorNumeric(palette = "viridis", domain = gcircles_routes$n, reverse = TRUE)
+pal <- colorNumeric(palette = "viridis", domain = routes$n, reverse = TRUE)
 
 # Labels: make separate labels for sources and destinations to avoid mismatches
 label1 <- sprintf(
   "%s to %s<br/>Distance: %g miles<br/>Letters: %g",
-  gcircles_routes$source, gcircles_routes$destination, gcircles_routes$miles, gcircles_routes$n
+  routes$source, routes$destination, routes$miles, routes$n
 ) |> lapply(htmltools::HTML)
 
 label2 <- sprintf(
   "<strong>%s</strong><br/>Letters from: %g<br/>Letters to: %g<br/>Correspondents: %g",
-  destinations$place, destinations$source, destinations$destination, destinations$correspondents
+  destinations$placename, destinations$source, destinations$destination, destinations$correspondents
 ) |> lapply(htmltools::HTML)
 
 label3 <- sprintf(
   "<strong>%s</strong><br/>Letters from: %g<br/>Letters to: %g<br/>Correspondents: %g",
-  sources$place, sources$source, sources$destination, sources$correspondents
+  sources$placename, sources$source, sources$destination, sources$correspondents
 ) |> lapply(htmltools::HTML)
 
 # Plot
-leaflet(gcircles_routes) |> 
+leaflet(routes) |> 
   addProviderTiles(providers$CartoDB.PositronNoLabels) |>
-  addCircleMarkers(data = destinations, lng = ~lon, lat = ~lat,
+  addCircleMarkers(data = destinations, lng = ~lng, lat = ~lat,
       color = "#31a354", stroke = FALSE, fillOpacity = 1, radius = 8, group = "Destinations",
       label = label2,
       labelOptions = labelOptions(textsize = "11px")) |>
-  addCircleMarkers(data = sources, lng = ~lon, lat = ~lat,
+  addCircleMarkers(data = sources, lng = ~lng, lat = ~lat,
       color = "#feb24c", stroke = FALSE, fillOpacity = 1, radius = 5, group = "Sources",
       label = label3,
       labelOptions = labelOptions(textsize = "11px")) |>
